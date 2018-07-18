b95e03a736090d512b0935413ed05f9407709f8e
bessemouelhi <bessem.mouelhi@outlook.fr>
2018-07-18 11:33:28 +0200

    update permission and bug fix

--- app/src/main/java/com/appdevloop/bessem/hikerswatch/MainActivity.java
+++ app/src/main/java/com/appdevloop/bessem/hikerswatch/MainActivity.java
@@ -2,6 +2,7 @@ package com.appdevloop.bessem.hikerswatch;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
@@ -13,9 +14,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,14 +40,17 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity {
 
     static final Integer LOCATION = 0x1;
+    public static final String TAG = "MainActivity";
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
 
     GoogleApiClient client;
     LocationRequest mLocationRequest;
     PendingResult<LocationSettingsResult> result;
     LocationManager locationManager;
+    LocationListener locationListener;
     String provider;
 
     TextView tv_alt;
@@ -64,18 +70,49 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
-        tv_alt = (TextView) findViewById(R.id.tv_altitude);
-        tv_lat = (TextView) findViewById(R.id.tv_latitude);
-        tv_lng = (TextView) findViewById(R.id.tv_longitude);
-        tv_speed = (TextView) findViewById(R.id.tv_speed);
-        tv_accuracy = (TextView) findViewById(R.id.tv_accuracy);
-        tv_address = (TextView) findViewById(R.id.tv_address);
+        tv_alt = findViewById(R.id.tv_altitude);
+        tv_lat = findViewById(R.id.tv_latitude);
+        tv_lng = findViewById(R.id.tv_longitude);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_address = findViewById(R.id.tv_address);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        provider = locationManager.getBestProvider(new Criteria(), false);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i(TAG, "onLocationChanged: " + location.toString());
+                updateLocationInfo(location);
+            }
 
-        //Location location = locationManager.getLastKnownLocation(provider);
-        //onLocationChanged(location);
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            startListening();
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    updateLocationInfo(location);
+                }
+            }
+        }
     }
 
     public static boolean isConnecting(Context context) {
@@ -91,60 +128,71 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-                    //Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    //Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+        }
+
+    }
+
+    public void startListening() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+
+    }
+
+    public void updateLocationInfo(Location location) {
+
+        Log.i("LocationInfo", location.toString());
+
+        tv_lat.setText("Latitude: " + String.format("%.5f", location.getLatitude()));
+        tv_lng.setText("Longitude: " + String.format("%.5f", location.getLongitude()));
+        tv_alt.setText("Altitude: " + String.format("%.0f", location.getAltitude()) + "m");
+        tv_accuracy.setText("Accuracy: " + location.getAccuracy());
+        tv_speed.setText("Speed: " + String.format("%.1f", location.getSpeed()) + "m/s");
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            String address = "Could not find address";
+            List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (listAddresses != null && listAddresses.size() > 0 ) {
+                Log.i("PlaceInfo", listAddresses.get(0).toString());
+                address = "Address: \n";
+
+                if (listAddresses.get(0).getSubThoroughfare() != null) {
+                    address += listAddresses.get(0).getSubThoroughfare() + " ";
                 }
-                //return;
+
+                if (listAddresses.get(0).getThoroughfare() != null) {
+                    address += listAddresses.get(0).getThoroughfare() + "\n";
+                }
+
+                if (listAddresses.get(0).getLocality() != null) {
+                    address += listAddresses.get(0).getLocality() + "\n";
+                }
+
+                if (listAddresses.get(0).getPostalCode() != null) {
+                    address += listAddresses.get(0).getPostalCode() + "\n";
+                }
+
+                if (listAddresses.get(0).getCountryName() != null) {
+                    address += listAddresses.get(0).getCountryName() + "\n";
+                }
             }
-            // other 'case' lines to check for other
-            // permissions this app might request
+            TextView addressTextView = findViewById(R.id.tv_address);
+            addressTextView.setText(address);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        //Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        locationManager.removeUpdates(this);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        onRestart();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        provider = locationManager.getBestProvider(new Criteria(), false);
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
-
-    }
-
-    @Override
+    /*@Override
     public void onLocationChanged(Location location) {
         Double alt = location.getAltitude();
         Double lat = location.getLatitude();
@@ -182,12 +230,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         tv_speed.setText(getString(R.string.speed) + String.format("%.1f", speed) + "m/s");
         tv_accuracy.setText(getString(R.string.acc) + acc + "m");
 
-        Log.i("Altitude", alt + "");
-        Log.i("Latitude", lat + "");
-        Log.i("Longitude", lng + "");
-        Log.i("Accuracy", acc + "");
-        Log.i("Speed", speed + "");
-        Log.i("Bearing", bearing + "");
+        Log.i(TAG, "Altitude " + alt + "");
+        Log.i(TAG, "Latitude " + lat + "");
+        Log.i(TAG, "Longitude " + lng + "");
+        Log.i(TAG, "Accuracy " + acc + "");
+        Log.i(TAG, "Speed " + speed + "");
+        Log.i(TAG, "Bearing " + bearing + "");
     }
 
     @Override
@@ -203,5 +251,5 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onProviderDisabled(String provider) {
 
-    }
+    }*/
 }
